138da0a3a074614b6a7000263be6c02acac13863
Soul <soul@Maccy.local>
2026-02-08 16:05:11 +0300

    Ramadan Greeting

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('greetingCanvas');
 const ctx = canvas.getContext('2d');
 const baseImage = new Image();
-baseImage.src = 'ITQAN_eidadha.jpeg';
+baseImage.src = 'ITQAN_ramadan.jpeg';
 
 const drawImageWithName = (name = '') => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -9,7 +9,7 @@ const drawImageWithName = (name = '') => {
   ctx.font = 'bold 36px Cairo';
   ctx.fillStyle = 'white';
   ctx.textAlign = 'center';
-  ctx.fillText(name, 570, 1060); // Fixed coordinates
+  ctx.fillText(name, 650, 1100); // Fixed coordinates
 };
 
 baseImage.onload = () => {
@@ -23,7 +23,7 @@ document.getElementById('nameInput').addEventListener('input', e => {
 });
 
 const downloadImage = () => {
-  const name = document.getElementById('nameInput').value.trim() || 'eid-greeting';
+  const name = document.getElementById('nameInput').value.trim() || 'ramadan-greeting';
   const link = document.createElement('a');
   link.download = `${name}.png`;
   link.href = canvas.toDataURL();
